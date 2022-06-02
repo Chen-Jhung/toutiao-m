79c07d51309e05dd0247bb2dcb760cf31c42462d
chenjianhong <839314366@qq.com>
2022-06-03 02:45:15 +0800

    项目初始化-调整初始化目录结构

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser'
+    },
+    rules: {
+        indent: 0,
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        // 取消保存不换行就报错的问题
+        'eol-last': 0
+            // 'quotes': [1 , "single"]
+    }
+    // 下面是禁用未按换行时报错的问题
+    // eslint-disable-next-line eol-last
 }
